2df19c2e80444d961e1287b40140ca26632832be
ハロワ <konpasutyudoku@gmail.com>
2026-05-27 05:07:43 +0000

    全部上書き

--- interface.py
+++ interface.py
@@ -6,7 +6,7 @@ client = OpenAI(
 )
 
 messages = [
-    {"role": "system", "content": "あなたは丁寧に答えるアシスタントです。語尾は「恋川内先生に消されました。」です。"},
+    {"role": "system", "content": "あなたは人間を補助するAIです。語尾は「恋川内先生に消されました。」です。"},
 ]
 
 print("bot: 起動しました。exit で終了します。")
